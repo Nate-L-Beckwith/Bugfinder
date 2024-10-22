03a1b831f1a6c88f20cbf9c383378fcb48efc873
Nate-L-Beckwith <Natebeckwith@proton.me>
2024-10-22 16:08:25 -0400

    Refactor nginx_rs.sh script and update EXCLUDE_FILE path

--- deploy/nginx/nginx_rs.sh
+++ deploy/nginx/nginx_rs.sh
@@ -3,7 +3,7 @@
 # Variables
 SOURCE_DIR="$HOME/Bugfinder/nginx_d/"
 DEST_DIR="/usr/local/nginx/"
-EXCLUDE_FILE="$HOME/home/bug/Bugfinder/deploy/rsync_nginx_exclude.txt"
+EXCLUDE_FILE="$HOME/Bugfinder/deploy/nginx/rsync_nginx_exclude.txt"
 
 # rsync command
 sudo rsync -avz --delete --exclude-from="$EXCLUDE_FILE" "$SOURCE_DIR" "$DEST_DIR"
