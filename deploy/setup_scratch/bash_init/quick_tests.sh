e46dd7e9af77dea420de99a0e3c2ce75b8636802
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 12:11:35 -0400

    Refactor quick_tests.sh to combine 'git restore *' and 'git pull' for streamlined execution

--- deploy/setup_scratch/bash_init/quick_tests.sh
+++ deploy/setup_scratch/bash_init/quick_tests.sh
@@ -2,11 +2,7 @@
 #!/bin/bash
 export bash_DEPLOY_DIR="$HOME/Bugfinder/deploy/setup_scratch/bash_init"
 
-git restore *
-
-
-
-git pull --rebase
+git restore * && git pull
 chmod +x $bash_DEPLOY_DIR/*.sh
 cp -rav $bash_DEPLOY_DIR/quick_tests.sh ~/quick_tests.sh
 sudo chmod +x ~/quick_tests.sh
