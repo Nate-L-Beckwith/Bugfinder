6c04addb16f915f75d440e7912daff6cbd9e1f61
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 12:30:42 -0400

    Switch to rtmp-breakout branch in quick_tests.sh before restoring and pulling changes

--- deploy/setup_scratch/bash_init/quick_tests.sh
+++ deploy/setup_scratch/bash_init/quick_tests.sh
@@ -2,6 +2,8 @@
 #!/bin/bash
 export bash_DEPLOY_DIR="$HOME/Bugfinder/deploy/setup_scratch/bash_init"
 
+git switch rtmp-breakout
+
 git restore * && git pull
 chmod +x $bash_DEPLOY_DIR/*.sh
 cp -rav $bash_DEPLOY_DIR/quick_tests.sh ~/quick_tests.sh
