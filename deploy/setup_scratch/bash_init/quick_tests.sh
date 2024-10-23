e69cf3823b8d1d087f4e4a3fb0f7277f9c6fa048
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 12:10:29 -0400

    Replace 'git pull' with 'git restore *' in quick_tests.sh for improved file management

--- deploy/setup_scratch/bash_init/quick_tests.sh
+++ deploy/setup_scratch/bash_init/quick_tests.sh
@@ -2,9 +2,10 @@
 #!/bin/bash
 export bash_DEPLOY_DIR="$HOME/Bugfinder/deploy/setup_scratch/bash_init"
 
-# Restore all files to the last committed state
-# Pull the latest changes and rebase
-git pull --rebase
+git restore *
+
+
+
 git pull --rebase
 chmod +x $bash_DEPLOY_DIR/*.sh
 cp -rav $bash_DEPLOY_DIR/quick_tests.sh ~/quick_tests.sh
