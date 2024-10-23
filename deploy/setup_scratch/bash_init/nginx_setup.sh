f77b88721911fe28783545f9424d59fefa82a835
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 10:20:01 -0400

    Remove obsolete NGINX configuration files and add setup scripts for NGINX installation

---
+++ deploy/setup_scratch/bash_init/nginx_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+include ./vars.sh
+include ../preres.sh
+
+sudo ../preres.sh
+sudo ./vars.sh
+
+sudo mkdir -p /src/nginx/modules
+
+cd $NGINX_SRC_DIR
+
+sudo wget http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
+sudo tar -zxvf nginx-$NGINX_VERSION.tar.gz
+sudo git clone https://github.com/arut/nginx-rtmp-module.git modules/nginx-rtmp-module
+
+cd $NGINX_DIR
+
+def
+sudo ./configure \
+    --prefix=/usr/local/nginx \
+    --sbin-path=/usr/sbin/nginx \
+    --conf-path=/etc/nginx/nginx.conf \
+    --pid-path=/var/run/nginx.pid \
+    --lock-path=/var/lock/nginx.lock \
+    --error-log-path=/var/log/nginx/error.log \
+    --http-log-path=/var/log/nginx/access.log \
+    --with-http_ssl_module \
+    --with-http_v2_module \
+    --with-http_realip_module \
+    --with-http_stub_status_module \
+    --with-http_sub_module \
+    --with-http_flv_module \
+    --with-http_mp4_module \
+    --with-pcre \
+    --with-pcre-jit \
+    --with-threads \
+    --with-stream \
+    --with-stream_ssl_module \
+    --add-module=../nginx-rtmp-module
+
+sudo make
+sudo make install
+
+sudo nginx -v
+
+echo "Done"
---
+++ deploy/setup_scratch/bash_init/vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export NGINX_VERSION="1.27.2"
+export NGINX_DIR="/src/nginx/nginx-$NGINX_VERSION"
+export NGINX_MODULE_DIR="/src/nginx/modules"
+export NGINX_SRC_DIR="/src/nginx"
+
+echo $NGINX_VERSION $NGINX_DIR $NGINX_MODULE_DIR $NGINX_SRC_DIR
