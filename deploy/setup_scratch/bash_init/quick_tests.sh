186c8251b2489589081c44702f21190e2727cb69
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 12:35:14 -0400

    Ensure correct branch switch in quick_tests.sh before restoring and pulling changes

--- deploy/setup_scratch/bash_init/quick_tests.sh
+++ deploy/setup_scratch/bash_init/quick_tests.sh
@@ -5,6 +5,7 @@ cd ~/Bugfinder
 git switch rtmp-breakout
 
 git restore * && git pull
+git switch rtmp-breakout
 chmod +x $bash_DEPLOY_DIR/*.sh
 cp -rav $bash_DEPLOY_DIR/quick_tests.sh ~/quick_tests.sh
 sudo chmod +x ~/quick_tests.sh
