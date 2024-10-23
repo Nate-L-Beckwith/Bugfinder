e3ab79e216b8603e04a82f25761c9a5e1fc25397
Nate-L-Beckwith <natebeckwith@proton.me>
2024-10-23 12:06:44 -0400

    Fix variable casing in quick_tests.sh for consistency and improve script execution

--- deploy/setup_scratch/bash_init/quick_tests.sh
+++ deploy/setup_scratch/bash_init/quick_tests.sh
@@ -1,11 +1,11 @@
 # quick_tests.sh
 #!/bin/bash
-export BASH_DEPLOY_DIR="$HOME/Bugfinder/deploy/setup_scratch/bash_init"
+export bash_DEPLOY_DIR="$HOME/Bugfinder/deploy/setup_scratch/bash_init"
 
 # Restore all files to the last committed state
 # Pull the latest changes and rebase
 git pull --rebase
 git pull --rebase
-chmod +x $BASH_DEPLOY_DIR/*.sh
+chmod +x $bash_DEPLOY_DIR/*.sh
 cp -rav $bash_DEPLOY_DIR/quick_tests.sh ~/quick_tests.sh
 sudo chmod +x ~/quick_tests.sh
